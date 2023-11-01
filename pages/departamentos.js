983d4a27f19bbd92e75b4b2cb350c93c40371e3b
Luis Angel Hernandez <rlwev@hotmail.com>
2023-11-01 17:53:09 -0500

    Se creó el Header y Footer

--- pages/departamentos.js
+++ pages/departamentos.js
@@ -1,10 +1,14 @@
-import Link from "next/link"
+import Link from "next/link";
+import Layout from "@/components/layout";
 
 export default function Departamentos() {
   return (
-    <>
-    <div>Departamentos aqui</div>
-    <Link href="/">Inicio</Link>
-    </>
-  )
+    <Layout
+      title={"Departamentos"}
+      description={"Encuentra los mejores departamentos en la Riviera Maya, Los Cabos y Mérida. Descubre la belleza natural del Caribe mexicano, la tranquilidad de la península de Baja California y la rica cultura de Yucatán. Encuentra opciones en diferentes zonas turísticas, tamaños y precios. ¡Vive en el departamento de tus sueños en México hoy mismo!"}
+    >
+      <div>Departamentos aqui</div>
+      <Link href="/">Inicio</Link>
+    </Layout>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import Link from "next/link"
+import Link from "next/link";
+import Layout from "@/components/layout";
 
 export default function Home() {
   return (
-    <>
+    <Layout
+      title={"Inicio"}
+      description={"Tulumfromsky Realtors es líder en la comercialización de desarrollos inmobiliarios en la Riviera Maya, Mérida y Los Cabos."}
+    >
       <h1>Hola mundo en Next</h1>
       <Link href="/departamentos">Depas</Link>
-    </>
-  )
+    </Layout>
+  );
 }
