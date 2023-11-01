cefd2301a9b2de58f2cf14c16cae826ae71674ac
Luis Angel Hernandez <rlwev@hotmail.com>
2023-11-01 17:01:42 -0500

    Se agregó la fuente al document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es">
+      <Head>
+        <meta name="description" content="Tulumfromsky Realtors es líder en la comercialización de desarrollos inmobiliarios en la Riviera Maya, Mérida y Los Cabos."/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'true'}/>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
---
+++ pages/departamentos.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Departamentos() {
+  return (
+    <>
+    <div>Departamentos aqui</div>
+    <Link href="/">Inicio</Link>
+    </>
+  )
+}
