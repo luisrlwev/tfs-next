9fe62463dd4583f681fde506d8ff5807b71c9f89
Luis Angel Hernandez <rlwev@hotmail.com>
2023-11-03 17:57:13 -0500

    se terminó el footer y el header

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,66 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import { FaFacebookF, FaYoutube, FaInstagram, FaXTwitter, FaPinterestP, FaTiktok, FaLinkedinIn, FaWhatsapp } from "react-icons/fa6";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <div className="bg-header py-10">
+        <div className="grid grid-cols-3 gap-3 container xl mx-auto">
+          <div>
+            <h3 className="text-tfs text-center text-xl uppercase font-bold letter-spacing-25 pb-2">Contáctanos</h3>
+            <form>
+              <div className="grid grid-cols-2 gap-3">
+                <div>
+                  <input type="text" name="nombre" id="nombre" placeholder="Nombre:" className="w-full rounded-md bg-cuarto text-white p-3" required/>
+                </div>
+                <div>
+                  <input type="tel" name="tel" id="tel" placeholder="Teléfono:" className="w-full rounded-md bg-cuarto text-white p-3" required/>
+                </div>
+                <div className="col-span-2">
+                  <input type="email" name="email" id="email" placeholder="Email:" className="w-full rounded-md bg-cuarto text-white p-3" required/>
+                </div>
+                <div className="col-span-2">
+                  <textarea name="mensaje" id="mensaje" cols="30" rows="3" placeholder="¿Qué tipo de propiedad buscas?" className="w-full rounded-md bg-cuarto text-white p-3" required></textarea>
+                </div>
+                <div className="col-span-2 text-white">
+                  <input type="checkbox" name="condiciones" id="condiciones" required/> Acepto los <Link href="/aviso-de-privacidad" target="_blank" className="text-tfs hover">términos y condiciones</Link>
+                </div>
+                <div className="col-span-2 text-center">
+                  <input type="submit" name="" id="" className="py-2 px-12 bg-tfs uppercase text-white font-bold letter-spacing-25 rounded-md hover-bg"/>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className="grid content-center px-3">
+            <p className="text-center text-white uppercase font-bold letter-spacing-25 pb-8 w-full">Licencias Certificadas</p>
+            <div className="grid grid-cols-3 justify-items-center items-center">
+              <Image src={'/img/agente-inmobiliario-persona-juridica-2.png'} width={131} height={72} alt="Agente inmobiliario persona juridica"/>
+              <Image src={'/img/AMPI-los-cabos-2.png'} width={131} height={126} alt="AMPI Los Cabos"/>
+              <Image src={'/img/credencial-asesor-2.png'} width={131} height={76} alt="Credencial de asesor"/>
+            </div>
+          </div>
+          <div className="grid justify-center items-center">
+            <Link href={'/'} className="h-max"><Image src={'/img/logo.svg'} width={354} height={201} alt="Logo TFS"/></Link>
+            <div className="grid grid-cols-7 justify-items-center text-tfs text-3xl h-max">
+              <a href="https://www.youtube.com/@tulumfromskyrealtors5181" target="_blank"><FaYoutube className="hover"/></a>
+              <a href="https://www.facebook.com/tulumfromskyrealtors/" target="_blank"><FaFacebookF className="hover"/></a>
+              <a href="https://www.instagram.com/tulumfromsky/" target="_blank"><FaInstagram className="hover"/></a>
+              <a href="https://twitter.com/FromskyTulum" target="_blank"><FaXTwitter className="hover"/></a>
+              <a href="https://www.pinterest.com.mx/Tulumfromsky/" target="_blank"><FaPinterestP className="hover"/></a>
+              <a href="https://www.tiktok.com/@tulumfromsky" target="_blank"><FaTiktok className="hover"/></a>
+              <a href="https://www.linkedin.com/company/40900945/admin/" target="_blank"><FaLinkedinIn className="hover"/></a>
+            </div>
+            <div className="flex justify-center align-center text-tfs h-max">
+              <a href="https://wa.me/529848035450" className="flex items-center w-fit h-fit hover"><FaWhatsapp className="pr-1 text-xl"/> +52 (984) 803 5450</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="text-white text-center text-base py-4 bg-footer">
+        <p className="font-bold uppercase text-footer letter-spacing-25 pb-2">Tulumfromsky Realtors © 2023 Todos los derechos reservados JAH OLAM SA DE CV</p>
+        <Link href={'/aviso-de-privacidad'} className="underline">Aviso de privacidad</Link>
+      </div>
+    </footer>
+  );
 }
--- components/header.js
+++ components/header.js
@@ -1,74 +1,73 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Header() {
+
+  const router = useRouter()
+
   return (
     <header className="bg-header text-white p-4">
-      <div className="container mx-auto flex justify-between items-center text-menu">
+      <div className="container mx-auto flex justify-between items-center text-menu text-center font-semibold uppercase">
         <Link href={'/'}>
-          <Image
-            src="/img/logo.svg"
-            alt="Tulumfromsky Logo"
-            width={123}
-            height={70}
-          />
+          <Image src="/img/logo.svg" alt="Tulumfromsky Logo" width={123} height={70}/>
         </Link>
         <nav>
           <ul className="flex space-x-4">
             <li>
-              <Link href="/departamentos" className="px-1 py-2 rounded">
+              <Link href="/departamentos" className={ router.pathname === '/departamentos' ? 'text-tfs' : 'hover'}>
                 Departamentos
               </Link>
             </li>
             <li>
-              <Link href="/lotes" className="px-1 py-2 rounded">
+              <Link href="/lotes" className={ router.pathname === '/lotes' ? 'text-tfs' : 'hover'}>
                 Lotes
               </Link>
             </li>
             <li>
-              <Link href="/casas" className="px-1 py-2 rounded">
+              <Link href="/casas" className={ router.pathname === '/casas' ? 'text-tfs' : 'hover'}>
                 Casas
               </Link>
             </li>
             <li>
-              <Link href="/merida" className="px-1 py-2 rounded">
+              <Link href="/merida" className={ router.pathname === '/merida' ? 'text-tfs' : 'hover'}>
                 Mérida
               </Link>
             </li>
             <li>
-              <Link href="/los-cabos" className="px-1 py-2 rounded">
+              <Link href="/los-cabos" className={ router.pathname === '/los-cabos' ? 'text-tfs' : 'hover'}>
                 Los Cabos
               </Link>
             </li>
             <li>
-              <Link href="/constructora" className="px-1 py-2 rounded">
+              <Link href="/constructora" className={ router.pathname === '/constructora' ? 'text-tfs' : 'hover'}>
                 Constructora
               </Link>
             </li>
             <li>
-              <Link href="/nosotros" className="px-1 py-2 rounded">
+              <Link href="/nosotros" className={ router.pathname === '/nosotros' ? 'text-tfs' : 'hover'}>
                 Nosotros
               </Link>
             </li>
             <li>
-              <Link href="/blog" className="px-1 py-2 rounded">
+              <Link href="/blog" className={ router.pathname === '/blog' ? 'text-tfs' : 'hover'}>
                 Blog
               </Link>
             </li>
             <li>
-              <Link href="/contacto" className="px-1 py-2 rounded">
+              <Link href="/contacto" className={ router.pathname === '/contacto' ? 'text-tfs' : 'hover'}>
                 Contacto
               </Link>
             </li>
             <li>
-              <Link href="/franquicias" className="px-1 py-2 rounded">
+              <Link href="/franquicias" className={ router.pathname === '/franquicias' ? 'text-tfs' : 'hover'}>
                 Franquicias
               </Link>
             </li>
           </ul>
         </nav>
         <div className="flex">
-          <p className="lang-active">ES</p>|
+          <p className="text-tfs">ES</p>|
           <Link href={'/en/'}>EN</Link>
         </div>
       </div>
