0d36baeea5d2327f97254261100d6e81fb854bd0
Luis Angel Hernandez <rlwev@hotmail.com>
2023-11-17 15:47:53 -0500

    Se modificó el footer y el index para la versión responsive

--- components/footer.js
+++ components/footer.js
@@ -6,34 +6,34 @@ export default function Footer() {
   return (
     <footer>
       <div className="bg-header py-10">
-        <div className="grid grid-cols-3 gap-3 container xl mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-3 container mx-auto">
           <div>
             <h3 className="text-tfs text-center text-xl uppercase font-bold letter-spacing-25 pb-2">Contáctanos</h3>
             <form>
-              <div className="grid grid-cols-2 gap-3">
+              <div className="grid px-3 lg:grid-cols-2 gap-3">
                 <div>
                   <input type="text" name="nombre" id="nombre" placeholder="Nombre:" className="w-full rounded-md bg-cuarto text-white p-3" required/>
                 </div>
                 <div>
                   <input type="tel" name="tel" id="tel" placeholder="Teléfono:" className="w-full rounded-md bg-cuarto text-white p-3" required/>
                 </div>
-                <div className="col-span-2">
+                <div className="lg:col-span-2">
                   <input type="email" name="email" id="email" placeholder="Email:" className="w-full rounded-md bg-cuarto text-white p-3" required/>
                 </div>
-                <div className="col-span-2">
+                <div className="lg:col-span-2">
                   <textarea name="mensaje" id="mensaje" cols="30" rows="3" placeholder="¿Qué tipo de propiedad buscas?" className="w-full rounded-md bg-cuarto text-white p-3" required></textarea>
                 </div>
-                <div className="col-span-2 text-white">
+                <div className="lg:col-span-2 text-white">
                   <input type="checkbox" name="condiciones" id="condiciones" required/> Acepto los <Link href="/aviso-de-privacidad" target="_blank" className="text-tfs hover">términos y condiciones</Link>
                 </div>
-                <div className="col-span-2 text-center">
+                <div className="lg:col-span-2 text-center">
                   <input type="submit" name="" id="" className="py-2 px-12 bg-tfs uppercase text-white font-bold letter-spacing-25 rounded-md hover-bg"/>
                 </div>
               </div>
             </form>
           </div>
-          <div className="grid content-center px-3">
-            <p className="text-center text-white uppercase font-bold letter-spacing-25 pb-8 w-full">Licencias Certificadas</p>
+          <div className="grid content-center px-3 py-8">
+            <p className="text-center text-white uppercase font-bold letter-spacing-25 pb-4 lg:pb-8 w-full text-lg lg:text-base">Licencias Certificadas</p>
             <div className="grid grid-cols-3 justify-items-center items-center">
               <Image src={'/img/agente-inmobiliario-persona-juridica-2.png'} width={131} height={72} alt="Agente inmobiliario persona juridica"/>
               <Image src={'/img/AMPI-los-cabos-2.png'} width={131} height={126} alt="AMPI Los Cabos"/>
@@ -41,8 +41,8 @@ export default function Footer() {
             </div>
           </div>
           <div className="grid justify-center items-center">
-            <Link href={'/'} className="h-max"><Image src={'/img/logo.svg'} width={354} height={201} alt="Logo TFS"/></Link>
-            <div className="grid grid-cols-7 justify-items-center text-tfs text-3xl h-max">
+            <Link href={'/'} className="h-max pb-10"><Image src={'/img/logo.svg'} width={354} height={201} alt="Logo TFS"/></Link>
+            <div className="grid grid-cols-7 justify-items-center text-tfs text-3xl h-max pb-8 lg:pb-0">
               <a href="https://www.youtube.com/@tulumfromskyrealtors5181" target="_blank"><FaYoutube className="hover"/></a>
               <a href="https://www.facebook.com/tulumfromskyrealtors/" target="_blank"><FaFacebookF className="hover"/></a>
               <a href="https://www.instagram.com/tulumfromsky/" target="_blank"><FaInstagram className="hover"/></a>
@@ -58,8 +58,8 @@ export default function Footer() {
         </div>
       </div>
       <div className="text-white text-center text-base py-4 bg-footer">
-        <p className="font-bold uppercase text-footer letter-spacing-25 pb-2">Tulumfromsky Realtors © 2023 Todos los derechos reservados JAH OLAM SA DE CV</p>
-        <Link href={'/aviso-de-privacidad'} className="underline">Aviso de privacidad</Link>
+        <p className="font-bold uppercase text-footer letter-spacing-25 pb-2 text-xs lg:text-base">Tulumfromsky Realtors © 2023 Todos los derechos reservados JAH OLAM SA DE CV</p>
+        <Link href={'/aviso-de-privacidad'} className="underline text-xs lg:text-base">Aviso de privacidad</Link>
       </div>
     </footer>
   );
--- pages/index.js
+++ pages/index.js
@@ -9,32 +9,32 @@ export default function Home() {
     >
       <Link href={'/departamentos'} className="portada-tulum flex relative justify-center">
         <div className="h-full w-full top-0 left-0 absolute overflow-hidden z-0">
-          <video src="/video/riviera-maya.mp4" className="absolute video-fondo" autoPlay muted playsInline loop></video>
+          <video src="/video/riviera-maya.mp4" className="absolute video-fondo video object-cover" autoPlay muted playsInline loop></video>
         </div>
         <div className="overlay-tulum h-full w-full absolute">
         </div>
         <div className="video flex justify-center items-center relative">
-          <h1 className="text-white font-bold text-center uppercase text-6xl letter-spacing-25 text-shadow">Riviera Maya</h1>
+          <h1 className="text-white font-bold text-center uppercase text-3xl lg:text-6xl letter-spacing-25 text-shadow">Riviera Maya</h1>
         </div>
       </Link>
       <Link href={'/merida'} className="portada-merida-inicio flex relative justify-center">
         <div className="h-full w-full top-0 left-0 absolute overflow-hidden z-0">
-          <video src="/video/yucatan.mp4" className="absolute video-fondo" autoPlay muted playsInline loop></video>
+          <video src="/video/yucatan.mp4" className="absolute video-fondo video object-cover" autoPlay muted playsInline loop></video>
         </div>
         <div className="overlay-tulum h-full w-full absolute">
         </div>
         <div className="video flex justify-center items-center relative">
-          <h1 className="text-white font-bold text-center uppercase text-6xl letter-spacing-25 text-shadow">Mérida</h1>
+          <h1 className="text-white font-bold text-center uppercase text-3xl lg:text-6xl letter-spacing-25 text-shadow">Mérida</h1>
         </div>
       </Link>
       <Link href={'/los-cabos'} className="portada-cabos-inicio flex relative justify-center">
         <div className="h-full w-full top-0 left-0 absolute overflow-hidden z-0">
-          <video src="/video/los-cabos.mp4" className="absolute video-fondo" autoPlay muted playsInline loop></video>
+          <video src="/video/los-cabos.mp4" className="absolute video-fondo video object-cover" autoPlay muted playsInline loop></video>
         </div>
         <div className="overlay-tulum h-full w-full absolute">
         </div>
         <div className="video flex justify-center items-center relative">
-          <h1 className="text-white font-bold text-center uppercase text-6xl letter-spacing-25 text-shadow">Los Cabos</h1>
+          <h1 className="text-white font-bold text-center uppercase text-3xl lg:text-6xl letter-spacing-25 text-shadow">Los Cabos</h1>
         </div>
       </Link>
     </Layout>
