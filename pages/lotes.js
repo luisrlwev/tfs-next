dd9a40a8d27a62ce1b83528d1f2c11f1a3290c09
Luis Angel Hernandez <rlwev@hotmail.com>
2023-12-14 11:04:51 -0500

    Se tradujo lotes al inglés

--- pages/lotes.js
+++ pages/lotes.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
 import Layout from "@/components/layout";
 import Image from "next/image";
 import Boton from "@/components/boton";
@@ -10,6 +12,8 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 
 export default function Lotes() {
+  // Traducciones
+  const { t } = useTranslation()
   const [showModal, setShowModal] = useState(false);
 
   const abrirModal = () => {
@@ -21,26 +25,26 @@ export default function Lotes() {
   };
   return (
     <Layout
-      title={"Lotes"}
-      description={"Descubre los mejores lotes en la Riviera Maya, Los Cabos y Mérida para construir la casa de tus sueños o invertir en el futuro. Disfruta de la belleza natural de México y las oportunidades de inversión en diferentes zonas turísticas. Encuentra opciones en diferentes tamaños y precios. ¡Aprovecha esta oportunidad única para construir la casa de tus sueños en México hoy mismo!"}
+      title={t('lotes')}
+      description={t('lotes_descripcion')}
     >
       <div className="portada-lotes flex relative justify-center cursor-pointer" onClick={abrirModal}>
         <div className="h-full w-full top-0 left-0 absolute overflow-hidden z-0">
           <video src="/video/lotes-portada.mp4" className="absolute video-fondo-portada" autoPlay muted playsInline loop></video>
         </div>
         <div className="portada-video flex justify-center items-center relative">
-          <h1 className="text-white font-bold text-center uppercase text-40 max-lg:text-3xl letter-spacing-25 text-shadow">Terrenos cerca del mar</h1>
+          <h1 className="text-white font-bold text-center uppercase text-40 max-lg:text-3xl letter-spacing-25 text-shadow">{t('terrenos')}</h1>
         </div>
       </div>
       <section className="bg-header">
         <div className="container mx-auto grid justify-center py-10 uppercase text-2xl max-lg:text-base font-bold letter-spacing-25">
-          <p className="w-full text-center text-white">Lotes desde: <span className="text-tfs"> $110,000 usd</span></p>
+          <p className="w-full text-center text-white">{t('lotes_desde')} <span className="text-tfs"> $110,000 usd</span></p>
         </div>
       </section>
       <section className="py-16">
         <div className="container mx-auto grid grid-cols-2 max-lg:grid-cols-1 max-lg:px-3 gap-4">
           <div className="max-lg:pb-8">
-            <p className="text-tfs uppercase font-bold text-xl letter-spacing-25 text-center pb-7">Lotes residenciales con <br/>campo de golf</p>
+            <p className="text-tfs uppercase font-bold text-xl letter-spacing-25 text-center pb-7">{t('lotes_residenciales')} <br/>{t('campo')}</p>
             <Swiper
               slidesPerView={1}
               navigation={true}
@@ -90,10 +94,10 @@ export default function Lotes() {
                 </div>
               </SwiperSlide>
             </Swiper>
-            <p className="py-5 px-7 bg-text max-lg:text-sm">Financiamiento hasta 9 años.</p>
+            <p className="py-5 px-7 bg-text max-lg:text-sm">{t('financiamiento')}</p>
           </div>
           <div>
-            <p className="text-tfs uppercase font-bold text-xl letter-spacing-25 text-center pb-7">Terrenos en el residencial más <br/>exclusivo de Tulum</p>
+            <p className="text-tfs uppercase font-bold text-xl letter-spacing-25 text-center pb-7">{t('terrenos_residencial')} <br/>{t('exclusivos')}</p>
             <Swiper
               slidesPerView={1}
               navigation={true}
@@ -143,7 +147,7 @@ export default function Lotes() {
                 </div>
               </SwiperSlide>
             </Swiper>
-            <p className="py-5 px-7 bg-text max-lg:text-sm">Club de playa privado.</p>
+            <p className="py-5 px-7 bg-text max-lg:text-sm">{t('club')}</p>
           </div>
         </div>
       </section>
@@ -151,15 +155,15 @@ export default function Lotes() {
         <div className="container mx-auto grid grid-cols-7 max-lg:grid-cols-2 gap-4">
           <div className="grid justify-items-center text-center">
             <Image src={'/img/departamentos/bares.svg'} className="pb-3" width={68} height={68} alt="bares y restaurantes"/>
-            <p>Bares y restaurantes</p>
+            <p>{t('bares')}</p>
           </div>
           <div className="grid justify-items-center text-center">
             <Image src={'/img/lotes/actividades-acuaticas.svg'} className="pb-3" width={68} height={68} alt="actividades acuáticas"/>
-            <p>Actividades acuáticas</p>
+            <p>{t('actividades')}</p>
           </div>
           <div className="grid justify-items-center text-center">
             <Image src={'/img/departamentos/yoga.svg'} className="pb-3" width={68} height={68} alt="zonas de yoga"/>
-            <p>Zonas de Yoga</p>
+            <p>{t('yoga')}</p>
           </div>
           <div className="grid justify-items-center text-center">
             <Image src={'/img/lotes/beach-club.svg'} className="pb-3" width={68} height={68} alt="beach club"/>
@@ -167,22 +171,22 @@ export default function Lotes() {
           </div>
           <div className="grid justify-items-center text-center">
             <Image src={'/img/lotes/cenotes.svg'} className="pb-3" width={68} height={68} alt="cenotes naturales"/>
-            <p>Cenotes naturales</p>
+            <p>{t('cenotes')}</p>
           </div>
           <div className="grid justify-items-center text-center">
             <Image src={'/img/lotes/ciclopistas.svg'} className="pb-3" width={68} height={68} alt="ciclopistas"/>
-            <p>Ciclopistas</p>
+            <p>{t('ciclopistas')}</p>
           </div>
           <div className="grid justify-items-center text-center max-lg:col-span-2">
             <Image src={'/img/lotes/campo-golf.svg'} className="pb-3" width={68} height={68} alt="campo de golf"/>
-            <p>Campo de Golf GPA</p>
+            <p>{t('golf')}</p>
           </div>
         </div>
       </section>
       <section className="bg-sexto">
         <div className="container mx-auto text-center py-8 text-black uppercase font-bold letter-spacing-25 text-2xl max-lg:text-base max-lg:px-3">
-          <p>Invierte en tu departamento con vista al</p>
-          <p>mar en la Riviera Maya</p>
+          <p>{t('invierte')}</p>
+          <p>{t('mar')}</p>
         </div>
       </section>
       <div className="portada-lotes-final flex relative justify-center cursor-pointer" onClick={abrirModal}>
@@ -192,7 +196,7 @@ export default function Lotes() {
         <div className="portada-video-final flex justify-center items-center relative">
           <div className="grid grid-cols-1 justify-items-center">
             <div>
-              <h3 className="text-white font-bold text-center uppercase text-xl letter-spacing-25 text-shado pb-5 max-lg:px-4">Exclusividad y lujo en la playa</h3>
+              <h3 className="text-white font-bold text-center uppercase text-xl letter-spacing-25 text-shado pb-5 max-lg:px-4">{t('exclusividad')}</h3>
             </div>
             <div>
               <Boton/>
@@ -203,4 +207,16 @@ export default function Lotes() {
       <Formulario show={showModal} onClose={cerrarModal}/>
     </Layout>
   );
+}
+
+export async function getStaticProps(context) {
+  // extract the locale identifier from the URL
+  const { locale } = context
+
+  return {
+    props: {
+      // pass the translation props to the page component
+      ...(await serverSideTranslations(locale)),
+    },
+  }
 }
