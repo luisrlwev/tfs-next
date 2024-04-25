4ade1e6fd9c5ff1e6ef5d00d56c1427817e9ed9e
Luis Angel Hernandez <rlwev@hotmail.com>
2024-04-25 12:03:24 -0500

    se agregaron las fotos y videos al home

---
+++ components/galeria-6-rios.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import Image from "next/image";
+import Lightbox from "yet-another-react-lightbox";
+import Video from "yet-another-react-lightbox/plugins/video";
+import 'yet-another-react-lightbox/styles.css';
+
+
+// Define tus slides aquí
+const slides = [
+    { src: '/img/index/6-rios/6-rios-1.jpg', type: 'image' },
+    { src: '/img/index/6-rios/6-rios-2.jpg', type: 'image' },
+    { src: '/video/6-rios.mp4', type: 'video', poster: '/img/index/6-rios/6-rios-3.jpg' },
+  ];
+
+export default function Galeria6Rios() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(0);
+  return (
+    <>
+        <div className="grid grid-rows-2 grid-flow-col gap-4 h-600">
+                {slides.map((slide, index) => (
+                    <div key={index} className={`${index === 0 ? "row-span-1" : index === 1 ? "col-span-1" : "row-span-2 col-span-1"} cursor-pointer relative`}>
+                        <Image
+                            src={slide.type === 'image' ? slide.src : slide.poster}
+                            alt={`Slide ${index}`}
+                            onClick={() => {
+                                setCurrentIndex(index);
+                                setIsOpen(true);
+                            }}
+                            layout="fill"
+                        />
+                    </div>
+                ))}
+            </div>
+
+            {isOpen && (
+                <Lightbox
+                    plugins={[Video]}
+                    slides={slides.map(slide => ({
+                        ...slide,
+                        src: slide.src,
+                        type: slide.type === 'video' ? 'video' : 'image',
+                        sources: [
+                            {
+                              src: "/video/6-rios.mp4",
+                              type: "video/mp4",
+                            },
+                        ],
+                    }))}
+                    open={isOpen}
+                    index={currentIndex}
+                    close={() => setIsOpen(false)}
+                />
+            )}
+    </>
+  )
+}
